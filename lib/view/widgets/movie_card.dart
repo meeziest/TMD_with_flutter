10365eeed0e01e99f6416c282431cc895f6e9cb5
Dimondi <dideful@mail.ru>
2022-02-17 23:36:54 +0600

    Hope last commit!

--- lib/view/widgets/movie_card.dart
+++ lib/view/widgets/movie_card.dart
@@ -19,6 +19,16 @@ class MovieCard extends StatelessWidget {
     required this.movieId})
   :super(key: key);
 
+  void openMovieDetail(int id, BuildContext context){
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MovieDetailScreen(
+            movieId: movieId),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -26,78 +36,78 @@ class MovieCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
       ),
-      child: Stack(
-        alignment: Alignment.bottomLeft,
-        children: [
-          Positioned(
-            right: 5,
-            top: 5,
-            child: Container(
-              width: 44,
-              height: 44,
-              decoration: const BoxDecoration(
-                  color: CustomColors.ratingCircleBackgroundColor,
-                  shape: BoxShape.circle
-              ),
-              child: CircularPercentIndicator(
-                fillColor: Colors.transparent,
-                backgroundColor: Colors.transparent,
-                radius: 17.0,
-                lineWidth: 3.0,
-                percent: voteAverage/10,
-                center: Text('$voteAverage'),
-                progressColor: Colors.white,
-              ),
-            ),
-          ),
-          Ink.image(
-            image: NetworkImage(posterPath),
-            height: 214.0,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MovieDetailScreen(
-                        movieId: movieId),
-                  ),
-                );              },
-            ),
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            child: Container(
-              height: 100.0,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  gradient: kMovieCardPosterGradient,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  movieName,
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
+      child: GestureDetector(
+        onTap: (){
+          openMovieDetail(movieId, context);
+        },
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            Positioned(
+              right: 5,
+              top: 5,
+              child: Container(
+                width: 44,
+                height: 44,
+                decoration: const BoxDecoration(
+                    color: CustomColors.ratingCircleBackgroundColor,
+                    shape: BoxShape.circle
                 ),
-                Text(
-                  releaseDate,
-                  style: const TextStyle(
-                      fontSize: 13.0,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xBFffffff)
-                  ),
-                )
-              ],
+                child: CircularPercentIndicator(
+                  fillColor: Colors.transparent,
+                  backgroundColor: Colors.transparent,
+                  radius: 17.0,
+                  lineWidth: 3.0,
+                  percent: voteAverage/10,
+                  center: Text('$voteAverage'),
+                  progressColor: Colors.white,
+                ),
+              ),
             ),
-          ),
-        ],
+            Ink.image(
+              image: NetworkImage(posterPath),
+              height: 214.0,
+              child: InkWell(
+                onTap: () {
+                  openMovieDetail(movieId, context);
+                },
+              ),
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              child: Container(
+                height: 100.0,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    gradient: kMovieCardPosterGradient,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movieName,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    releaseDate,
+                    style: const TextStyle(
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xBFffffff)
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
